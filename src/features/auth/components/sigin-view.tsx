0b9402393aa71f85d0c3cef8099f8d14bfa3179b
truong.nt <truongnt0279@gmail.com>
2025-03-04 15:07:18 +0700

    update title

--- src/features/auth/components/sigin-view.tsx
+++ src/features/auth/components/sigin-view.tsx
@@ -21,7 +21,7 @@ export default function SignInViewPage({ stars }: { stars: number }) {
           'absolute right-4 top-4 hidden md:right-8 md:top-8'
         )}
       >
-        Login
+        Login here
       </Link>
       <div className='relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex'>
         <div className='absolute inset-0 bg-zinc-900' />
